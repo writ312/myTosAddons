07036b79d0180c2d31907172a1db4a0b5e385af2
natoto nakayama <nn67101460@gmail.com>
2016-12-16 02:02:36 +0900

    ver1.0

--- poisonpotautocharge/src/poisonpotautocharge.lua
+++ poisonpotautocharge/src/poisonpotautocharge.lua
@@ -1,67 +1,54 @@
 local acutil = require('acutil');
 
-_G['ADDONS'] = _G['ADDONS'] or {};
-poisonpotAutoCharge = _G["ADDONS"]["RESOURCEGAUGE"] or {};
-
-function POISONPOT_AUTO_CHARGE_ON_INIT(addon, frame)
-    poisonpotAutoCharge.addon = addon;
-	poisonpotAutoCharge.frame = frame;
-	
-	POISONPOT_AUTO_CHARGE_INIT();
-end
-
-function POISONPOT_AUTO_CHARGE_INIT()
-    ui.SysMsg("Load poisonpot auto charge")
-    acutil.slashCommand('/ppc',POISONPOT_AUTO_CHARGE);
-    acutil.setupEvent(poisonpotAutoCharge.addon,"FPS_UPDATE",POISONPOT_AUTO_CHARGE)
+function POISONPOTAUTOCHARGE_ON_INIT(addon, frame)
+    -- ui.SysMsg("Load poisonpot auto charge")
+    -- acutil.setupEvent(addon,"FPS_UPDATE","POISONPOT_AUTO_CHARGE")
+    -- acutil.slashCommand('/ppc',POISONPOT_AUTO_CHARGE);
+    POISONPOT_AUTO_CHARGE()
 end
 
 function POISONPOT_AUTO_CHARGE()
-    if hasPoisonJob == nil then
-        return;
-    end
-    
-    if not needPoisonCharge() then
-     return;
 
-    local poisonpot = GET_BAGWORMPOISON_ID()
-    local cnt = 1;
+    if hasPoisonJob() == nil then
+        return;end
+
+    local poisonAmount = getPoisonAmount()
+    if  poisonAmount == nil then
+        return;end
+
+    local poisonpot = GET_POISONPOT_ID();
+
+    if poisonpot == nil then
+        return;end
+
+    local cnt = math.floor((1000 - poisonAmount) / 200);
     session.AddItemID(poisonpot, cnt);    
     EXECUTE_POISONPOT_COMMIT()
 end
 
-local function hasPoisonJob()
+function hasPoisonJob()
 	local frame = ui.GetFrame("sysmenu");
 	local poisonpot = GET_CHILD(frame, "poisonpot", "ui::CButton");
-	if poisonpot ~= nil then
-		return true
+	if poisonpot == nil then
+		return nil
 	end
-	    return false
+	    return true
 end
 
-local function needPoisonCharge()
+function getPoisonAmount()
     local myEtc = GetMyEtcObject();
     local poisonAmount = myEtc['Wugushi_PoisonAmount'];
-    local poisonMaxAmount = myEtc['Wugushi_PoisonMaxAmount'];
-    if ((poisonMaxAmount - poisonAmount) < 500) then
-        return true
-    else
-        return false
+    ui.SysMsg(poisonAmount)
+    -- local poisonMaxAmount = myEtc['Wugushi_PoisonMaxAmount'];
+    if poisonAmount < 700 then
+        return poisonAmount;
     end
+    return nil
 end
 
 -- 645569 misc_poisonpot
-function GET_BAGWORMPOISON_ID()
-    local item = _GET_BAGWORMPOISON_ID()
 
-    if item == nil then
-        ui.SysMsg('dont exist poisonpot');
-        return nil;
-    end
-    return item;
-end
-
-function _GET_BAGWORMPOISON_ID()
+function GET_POISONPOT_ID()
     local inventoryItems = session.GetInvItemList();
 	
 	if inventoryItems == nil then
