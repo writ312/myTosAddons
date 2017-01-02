adbd84499c78d5c8351be602b01ed699b36c042d
natoto nakayama <nn67101460@gmail.com>
2017-01-03 08:56:55 +0900

    fix

--- hotkeyability/src/hotkeyability.lua
+++ hotkeyability/src/hotkeyability.lua
@@ -5,6 +5,7 @@ local g = _G['ADDONS']['HOTKEYABILITY'];
 g.user = nil;
 g.setting = {}
 g.settingPath = '../addons/hotkeyability/'
+
 CHAT_SYSTEM('on load hotkey')
 
 function HOTKEYABILITY_ON_INIT(addon,frame)
@@ -12,17 +13,23 @@ function HOTKEYABILITY_ON_INIT(addon,frame)
     acutil.setupHook(QUICKSLOTNEXPBAR_EXECUTE_HOOK,'QUICKSLOTNEXPBAR_EXECUTE')
     acutil.slashCommand('/hotkey', HOTKEYABILITY_COMMAND)
     local g = _G['ADDONS']['HOTKEYABILITY'];
+    g.addon = addon
+    g.frame = frame
+    frame:ShowWindow(1)
     if(g.user ~= GETMYPCNAME()) then
         g.setting = {}
         g.user = GETMYPCNAME()
     end
+  
     g.setting ,e = acutil.loadJSON(g.settingPath..GETMYPCNAME()..'.json',nil)
+  
     if(e) then
         g.setting = {}
     return;end
+
     if  g.setting then
         for k,v in pairs(g.setting) do
-            HOTKEYABILITY_SET_ICON(k,v[1])
+            HOTKEYABILITY_SET_ICON(k,GetAbilityData(v[1]))
         end
     end
 end
@@ -31,23 +38,30 @@ function HOTKEYABILITY_COMMAND(command)
     local acutil = require('acutil')
     local g = _G['ADDONS']['HOTKEYABILITY'];
     local key = table.remove(command,1);
+    
     if( key == 'd') then
         g.setting[table.remove(command,1)] = nil
         acutil.saveJSON(g.settingPath..GETMYPCNAME()..'.json',g.setting)
     end
-
+    
+    if(key == 'list') then
+        for k,v in pairs(g.setting) do
+            local abilID,abilName,abilClass = GetAbilityData(v[1])
+            CHAT_SYSTEM(string.foramt('%s : %s',k,abilClass.Name))
+        end
+        return
+    end
+    
     local num = tonumber(table.remove(command,1))
-    local abil = session.GetAbility(num);
-	if not abil then
-        CHAT_SYSTEM('無効な数値')
+
+    local abilID = GetAbilityData(num)
+	if not abilID then
+        CHAT_SYSTEM('error')
         return;
     end
-    local abilClass = GetIES(abil:GetObject());
-	local abilName = abilClass.ClassName;
-	local abilID = abilClass.ClassID;
-    g.setting[key] = {abilID,abilName}
+    g.setting[key] = {abilID}
     acutil.saveJSON(g.settingPath..GETMYPCNAME()..'.json',g.setting)
-    HOTKEYABILITY_SET_ICON(key,abilClass.ClassID)
+    HOTKEYABILITY_SET_ICON(key,abilID)
 end
 
 function QUICKSLOTNEXPBAR_EXECUTE_HOOK(number)
@@ -55,38 +69,54 @@ function QUICKSLOTNEXPBAR_EXECUTE_HOOK(number)
     local key = tostring(number + 1)
     local abil = g.setting[key]
     if abil then
-        HOTKEYABILITY_TOGGLE_ABILITIY(key,abil[1],abil[2])
+        HOTKEYABILITY_TOGGLE_ABILITIY(key,abil[1])
     else
         QUICKSLOTNEXPBAR_EXECUTE_OLD(number)
     end
 end
 
-function HOTKEYABILITY_TOGGLE_ABILITIY(key,abilID,abilName)
-    local abil =  session.GetAbility(abilID)
-    if not abil then
-    return;end
-    local abilClass = GetIES(abil:GetObject())
-    local frame = ui.GetFrame('quickslotnexpbar')
-    local slot = frame:GetChild('slot'..key)
-    local icon = CreateIcon(slot);	
-    icon:SetImage(abilClass.Icon);
+function HOTKEYABILITY_TOGGLE_ABILITIY(key,abilID)
+    local abilID,abilName,abilClass = GetAbilityData(abilID)
+    if not abilID then return end 
+
     local status = abilClass.ActiveState
+
+    local icon  = GetIcon(ui.GetFrame('quickslotnexpbar'):GetChild('slot'..key))
     icon:SetGrayStyle(status)
+
     local topFrame = ui.GetFrame('skilltree');
 	topFrame:SetUserValue("CLICK_ABIL_ACTIVE_TIME",imcTime.GetAppTime()-10);
     local fn = _G['TOGGLE_ABILITY_ACTIVE']
     fn(nil, nil,abilName,abilID)
+
 end
 
 function HOTKEYABILITY_SET_ICON(key,abilID)
-    local abil =  session.GetAbility(abilID)
-    if not abil then
-    return;end
-    local abilClass = GetIES(abil:GetObject())
+    local abilID,abilName,abilClass = GetAbilityData(abilID)
+    if not abilID then return end 
+
     local frame = ui.GetFrame('quickslotnexpbar')
     local slot = frame:GetChild('slot'..key)
     local icon = CreateIcon(slot);	
     icon:SetImage(abilClass.Icon);
+   
+    icon:SetTooltipType('ability');
+	icon:SetTooltipStrArg(abilClass.Name);
+	icon:SetTooltipNumArg(abilClass.ClassID);
+	local abilIES = GetAbilityIESObject(pc, abilClass.ClassName);
+	icon:SetTooltipIESID(GetIESGuid(abilIES));
+
     local status = abilClass.ActiveState
     icon:SetGrayStyle((status == 1) and 0 or 1)
+    return icon
+end
+
+ function GetAbilityData(abilID)
+    local abil = session.GetAbility(abilID);
+	if not abil then
+    return;end
+    
+    local abilClass = GetIES(abil:GetObject());
+	local abilName = abilClass.ClassName;
+    return abilID,abilName,abilClass;
 end
