2e8e279287c47f7de0e74f5e6c76a911e752914e
writ312 <writ0312@gmail.com>
2017-11-25 18:31:09 +0900

    fix

--- AccountBook/accountbook/accountbook.lua
+++ AccountBook/accountbook/accountbook.lua
@@ -2,36 +2,39 @@ _G['ADDONS'] = _G['ADDONS'] or {};
 _G['ADDONS']['ACCOUNTBOOK'] = _G['ADDONS']['ACCOUNTBOOK'] or {};
 local g = _G['ADDONS']['ACCOUNTBOOK'];
 local acutil = require('acutil');
-local today = os.date("%yy%mm%dd")
+local today = os.date("%y%m%d")
 local folderPath = '../addons/accountbook/'
 local isFirstLoad = true
-local today
 local todayIndex = 1
--- ログイン記録の保存
-g.loginRecord ,e = acutil.loadJSON(folderPath..'loginRecord.json',nil)  
-if(e) then
-    table.push(g.loginRecord,today)
-    acutil.saveJSON(folderPath ..'loginRecord.json',g.loginRecord)
-else
-    if #g.loginRecord < 3 then
-        table.push(g.loginRecord,today)
-    elseif g.loginRecord[3] ~= today then
-        table.remove(g.loginRecord,1)
-        table.push(g.loginRecord,today)
-    end
-end    
+local function accountBookInit()
+    -- ログイン記録の保存
+    g.loginRecord ,e = acutil.loadJSON(folderPath..'loginRecord.json',nil)  
+    if(e) then
+        g.loginRecord = {}
+        table.insert(g.loginRecord,today)
+        acutil.saveJSON(folderPath ..'loginRecord.json',g.loginRecord)
+    else
+        if #g.loginRecord < 3 then
+            table.insert(g.loginRecord,today)
+        elseif g.loginRecord[3] ~= today then
+            table.remove(g.loginRecord,1)
+            table.insert(g.loginRecord,today)
+        end
+    end    
 
--- 本日と過去のデータ読み込み又は作成
-g.accountBook = {}
-for i,date in ipars(g.loginRecord) do
-    if date then
-        local tempBook,e = acutil.loadJSON(folderPath..date..'.json')
-        g.accountBook[i] = tempBook or {}
-        acutil.saveJSON(folderPath..date..'.json',g.accountBook[i])
+    -- 本日と過去のデータ読み込み又は作成
+    g.accountBook = {}
+    for i,date in ipairs(g.loginRecord) do
+        if date then
+            local tempBook,e = acutil.loadJSON(folderPath..date..'.json')
+            g.accountBook[i] = tempBook or {}
+            acutil.saveJSON(folderPath..date..'.json',g.accountBook[i])
+        end
+        todayIndex = i
     end
-    todayIndex = i
-    today = date
 end
+
+accountBookInit()
 
 --jsonの構造とか
 -- yymmdd.json
@@ -49,25 +52,15 @@ end
 -- total:{
 --     writ:{
 --         expenses:100000,
---         incomes:500000
+--         incomes:500000,
+--         invMoney:1000000
 --     }
--- }
-g.totalAsset , e = acutil.loadJSON(folderPath..'totalAsset.json')
-if e then
-    g.totalAsset = {}
-    g.totalAsset.userMoeny = {}
-    acutil.saveJSON(folderPath..'totalAsset.json')
-end
--- totalAsset.json
--- {
---     deposit : 10000,
---     usersMoeny : {
---         hoge : 880
---     }
+-- deposit = 50000
 -- }
 
 -- init処理
 function ACCOUNTBOOK_ON_INIT(addon,frame)
+    
     ACCOUNTBOOK_CREATE_FRAME(frame)
     addon:RegisterMsg('INV_ITEM_ADD', 'ACCOUNTBOOK_UPDATE');
     addon:RegisterMsg('INV_ITEM_REMOVE', 'ACCOUNTBOOK_UPDATE');
@@ -79,23 +72,23 @@ function ACCOUNTBOOK_ON_INIT(addon,frame)
 
 --　一時帳簿をプッシュ 初回ならスルー
     if not isFirstLoad then
-        g.accountBook[todayIndex][g.user] = g.accountBook[todayIndex][g.user] or {}
-        table.push(g.accountBook[todayIndex][g.user],g.tempBook)
+        g.accountBook[todayIndex].users[g.user] = g.accountBook[todayIndex][g.user] or {}
+        table.insert(g.accountBook[todayIndex].users[g.user].g.tempBook)
         isFirstLoad = false
         acutil.saveJSON(folderPath..today..'.json',g.accountBook[todayIndex])
         
     end
     --キャラが違ったら所持金を保存し、総支出と総収入の計算
-    if not string.find(g.user ,GET_PC_MONEY() then
-        g.totalAsset.userMoeny[g.user] = g.money
+    if not string.find(g.user ,GET_PC_MONEY()) then
         local totalExpenses = 0
         local totalIncomes = 0
-        for i , item in ipars(g.accountbook.users[g.user]) do
+        for i , item in ipairs(g.accountbook[todayIndex].users[g.user]) do
             totalExpenses = totalExpenses + item.expenses
             totalIncomes = totalIncomes + item.incomes
         end
-        g.accountBook.total[g.user].expenses = totalExpenses
-        g.accountBook.total[g.user].incomes = totalIncomes
+        g.accountbook[todayIndex].total[g.user].invMoeny = g.money
+        g.accountBook[todayIndex].total[g.user].expenses = totalExpenses
+        g.accountBook[todayIndex].total[g.user].incomes = totalIncomes
     end
     g.user = GETMYPCNAME()
     g.money =  GET_PC_MONEY()
@@ -104,7 +97,7 @@ function ACCOUNTBOOK_ON_INIT(addon,frame)
     t.tempBook.map = g.map
 end
 
---
+--大体INDUNPLUSのパクリ。前のUIだったら無理やり引っ張れたのだが。。。
 function ACCOUNTBOOK_SAVE_WAREHOUSE_MONEY()
     local itemList = session.GetEtcItemList(IT_ACCOUNT_WAREHOUSE);
     local index = itemList:Head();
@@ -115,13 +108,15 @@ function ACCOUNTBOOK_SAVE_WAREHOUSE_MONEY()
         local invItem = itemList:Element(index);
         local obj = GetIES(invItem:GetObject());
         if obj.ClassName == MONEY_NAME then
-            g.totalAsset.deposit = deposit
-            acutil.saveJSON(folderPath..'totalAsset.json')    
-            return;
+            deposit = invItem.count
+            break;
         end
     index = itemList:Next(index);
     end
+    g.accountBook[todayIndex].deposit = deposit
+    acutil.saveJSON(folderPath..today..'.json',g.accountBook[todayIndex])
 end
+
 function ACCOUNTBOOK_UPDATE(frame, msg, guid, invIndex)
 -- vis == シルバー
     local obj = GetObjectByGuid(guid)
@@ -140,20 +135,18 @@ function ACCOUNTBOOK_UPDATE(frame, msg, guid, invIndex)
     end
 end
 
-local function accountBookCreateAssetGBox(frame,index,x,y)
-    local width , height = 300 , 200
-    local gbox = frame:CreateOrGetControl('groupbox','assetGbox'..index, 10 + (index - 1)%3*width, 10 + (index - 1)%3*height, width, height)
+
+function ACCOUNTBOOK_CREATE_TOTAL_ASSET_FRAME(frame,control,argStr,argNum)
+    local assetGbox = frame:GetChild('assetGbox')
+    assetGbox:RemoveAllChild()
+
 
 end
 
-function ACCOUNTBOOK_CREATE_ASSET_FRAME(frame,control,username,index)
-    local accountBook = g.accountBook[index][username]
-    local 
-end
-
+-- マップ名と収入、支出の表示作成。多分大きからあとで調整するの
 local function accountbookCreateAssetItem(gbox,username,index)
-    local accountBook = g.accountBook[index][username]
-    for i , item in ipars(accountBook) do
+    local accountBook = g.accountBook[todayIndex].users[index][username]
+    for i , item in ipairs(accountBook) do
         local itemGbox = gbox:CreateOrGetControl('groupbox','itemGbox'..i,10,(i-1)*100+10,180,90)
         local mapName = itemGbox:CreateOrGetControl('richtext','mapName',5,5,170,25)
         mapName:SetText(item.map) 
@@ -169,8 +162,13 @@ end
 function ACCOUNTBOOK_CREATE_USER_ASSET_FRAME(frame,control,username,argNum)
     -- local accountBook = g.accountBook[todayIndex][argStr]
     local assetGbox = frame:GetChild('assetGbox')
-    for i , date in ipars(g.loginRecord) do
-        local gbox = assetGbox:CreateOrGetControl('groupbox',date..'Gbox',(i-1)*200+10,10,200,600)
+    assetGbox:RemoveAllChild()
+    for i , date in ipairs(g.loginRecord) do
+        local gbox = assetGbox:CreateOrGetControl('groupbox',date..'Gbox',(i-1)*200+10,60,200,600)
+        local incomesText = itemGbox:CreateOrGetControl('richtext','TotalIncomesText',5,5,85,45)
+        incomesText:SetText('TOTAL_INCOMES{nl}'..g.accountBook[todayIndex].total[username].incomes)
+        local expensesText = itemGbox:CreateOrGetControl('richtext','TotalExpensesText',95,5,85,45)
+        expensesText:SetText('TOTAL_EXPENSES{nl}'..g.accountBook[todayIndex].total[username].expenses)
         accountbookCreateAssetItem(gbox,username,i)
     end
 end
@@ -182,8 +180,7 @@ function ACCOUNTBOOK_CREATE_USERSLIST_FRAME(frame,gbox)
     btn:SetEventScript(ui.LBTNUP,'ACCOUNTBOOK_CREATE_TOTAL_ASSET_FRAME')
     btn:SetText('Total')
     i = i  + 1
-    end
-    for username , v in pars(g.accountBook[todayIndex]) do
+    for username , v in pars(g.accountBook[todayIndex].users) do
         local btn = gbox:CreateOrGetControl('button','userBtn_'..username,10,30*i + 10,80,20)
         btn:SetEventScript(ui.LBTNUP,'ACCOUNTBOOK_CREATE_USER_ASSET_FRAME')
         btn:SetEventScriptArgString(ui.LBTNUP,username)
