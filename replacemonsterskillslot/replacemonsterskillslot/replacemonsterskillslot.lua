4ff2881446bd885595ac2e7b32e90d00be5c22b7
writ312 <writ0312@gmail.com>
2018-10-21 17:13:04 +0900

    v1.1

--- replacemonsterskillslot/replacemonsterskillslot/replacemonsterskillslot.lua
+++ replacemonsterskillslot/replacemonsterskillslot/replacemonsterskillslot.lua
@@ -2,20 +2,163 @@ _G['ADDONS'] = _G['ADDONS'] or {};
 _G['ADDONS']['REPLACEMONSTERSKILLSLOT'] = _G['ADDONS']['REPLACEMONSTERSKILLSLOT'] or {};
 local g = _G['ADDONS']['REPLACEMONSTERSKILLSLOT']
 local acutil = require('acutil')
--- g.setting = acutil.loadJSON('../addons/replacemonsterskillslot/setting.json') or {slotNum = 0}
--- acutil.saveJSON('../addons/replacemonsterskillslot/setting.json',g.setting)
+g.setting = acutil.loadJSON('../addons/replacemonsterskillslot/setting.json') or {slotNum = 0}
+acutil.saveJSON('../addons/replacemonsterskillslot/setting.json',g.setting)
+
+local function GetHotKeySetting(idx)
+	local obj = {}
+	obj.idx = idx
+	obj.key = config.GetHotKeyElementAttributeForConfig(idx, "Key")
+	obj.useAlt = config.GetHotKeyElementAttributeForConfig(idx, "UseAlt")
+	obj.useCtrl = config.GetHotKeyElementAttributeForConfig(idx, "UseCtrl")
+	obj.useShift = config.GetHotKeyElementAttributeForConfig(idx, "UseShift")
+	-- print(string.format("idx %d , key %s , alt %s , ctrl %s , shift %s ",obj.idx,obj.key,obj.useAlt,obj.useCtrl,obj.useShift))
+	return obj
+end
+
+function SaveHotKeySetting()
+	-- hoge foo あとで置換する
+	g.hoge = {}
+	for i = 0 , 4 do
+		table.insert(g.hoge,GetHotKeySetting(i))
+	end
+	g.foo = {}
+	for i = g.setting.slotNum , g.setting.slotNum + 4 do
+		table.insert(g.foo , GetHotKeySetting(i))
+	end
+	acutil.saveJSON('../addons/replacemonsterskillslot/setting.json',g.setting)
+end
+
+local function swapSlotItems(srcIndex,destIndex)
+	local frame= ui.GetFrame("quickslotnexpbar")
+	local srcSlot = GET_CHILD_RECURSIVELY(frame, "slot"..srcIndex, "ui::CSlot")
+	local destSlot = GET_CHILD_RECURSIVELY(frame, "slot"..destIndex, "ui::CSlot")
+	local src = {
+		index = srcIndex
+	}
+	local srcIcon = srcSlot:GetIcon()
+	
+	local dest = {
+		index = destIndex
+	}
+	local destIcon = destSlot:GetIcon()
+	if destIcon ~= nil then
+		local iconInfo = destIcon:GetInfo()
+		dest.category = iconInfo.category
+		dest.invIndex = iconInfo.ext
+		dest.type = iconInfo.type
+		dest.guid = iconInfo:GetIESID()
+	end
+	if srcIcon ~= nil then
+		SET_QUICK_SLOT(destSlot, src.category, src.type, src.guid, 1, true);
+	end
+	g.setting[g.user].src = src
+	g.setting[g.user].dest = dest
+end
+
+function changeTransformingStatus()
+	g.setting[g.user].transforming = false
+	acutil.saveJSON('../addons/replacemonsterskillslot/setting.json',g.setting)
+end
+
+local function restoreQuickSlotItem()
+	local dest = g.setting[g.user].dest
+	local frame= ui.GetFrame("quickslotnexpbar")
+	local destSlot = GET_CHILD_RECURSIVELY(frame, "slot"..dest.index, "ui::CSlot")
+	SET_QUICK_SLOT(destSlot, dest.category, dest.type, dest.guid, 1, true);
+
+	--キャラチェン対策(変更時に上記の処理が間に合わないため、フラグをOFFにさせない)
+	ReserveScript('changeTransformingStatus()',5.0)
+end
+
+local function swapQuickslotPos(srcIndex,destIndex)
+	local frame= ui.GetFrame("quickslotnexpbar")
+	local srcSlot = GET_CHILD_RECURSIVELY(frame, "slot"..srcIndex, "ui::CSlot")
+	local destSlot = GET_CHILD_RECURSIVELY(frame, "slot"..destIndex, "ui::CSlot")
+
+	local srcSlotMargin = srcSlot:GetMargin()
+	local destSlotMargin = destSlot:GetMargin()
+
+	local src = {
+		x = srcSlotMargin.left,
+		y = srcSlotMargin.top
+	}
+	local dest = {
+		x = destSlotMargin.left,
+		y = destSlotMargin.top
+	}
+	srcSlot:SetOffset(dest.x,dest.y)
+	destSlot:SetOffset(src.x,src.y)
+end
+
+--あとで処理をまとめる
+local function swapHotkeySetting(obj_1,obj_2,isChangePos)
+	local idx_1 = obj_1.idx
+	config.SetHotKeyElementAttributeForConfig(idx_1, "Key", obj_2.key);
+	config.SetHotKeyElementAttributeForConfig(idx_1, "UseAlt", obj_2.useAlt);
+	config.SetHotKeyElementAttributeForConfig(idx_1, "UseCtrl", obj_2.useCtrl);
+	config.SetHotKeyElementAttributeForConfig(idx_1, "UseShift", obj_2.useShift);
+
+	local idx_2 = obj_2.idx
+	config.SetHotKeyElementAttributeForConfig(idx_2, "Key", obj_1.key);
+	config.SetHotKeyElementAttributeForConfig(idx_2, "UseAlt", obj_1.useAlt);
+	config.SetHotKeyElementAttributeForConfig(idx_2, "UseCtrl", obj_1.useCtrl);
+	config.SetHotKeyElementAttributeForConfig(idx_2, "UseShift", obj_1.useShift);
+	
+	config.SaveHotKey('hotkey.xml')
+	local frame = ui.GetFrame('quickslotnexpbar')
+	QUICKSLOTNEXPBAR_UPDATE_HOTKEYNAME(frame);
+	frame:Invalidate()
+	if isChangePos then
+		swapQuickslotPos(idx_1 +1,idx_2 +1)
+	end
+end
+
+local function restoreHotkeySetting(obj_1,obj_2,isChangePos)
+	local idx_1 = obj_1.idx
+	config.SetHotKeyElementAttributeForConfig(idx_1, "Key", obj_1.key);
+	config.SetHotKeyElementAttributeForConfig(idx_1, "UseAlt", obj_1.useAlt);
+	config.SetHotKeyElementAttributeForConfig(idx_1, "UseCtrl", obj_1.useCtrl);
+	config.SetHotKeyElementAttributeForConfig(idx_1, "UseShift", obj_1.useShift);
+
+	local idx_2 = obj_2.idx
+	config.SetHotKeyElementAttributeForConfig(idx_2, "Key", obj_2.key);
+	config.SetHotKeyElementAttributeForConfig(idx_2, "UseAlt", obj_2.useAlt);
+	config.SetHotKeyElementAttributeForConfig(idx_2, "UseCtrl", obj_2.useCtrl);
+	config.SetHotKeyElementAttributeForConfig(idx_2, "UseShift", obj_2.useShift);
+	
+	config.SaveHotKey('hotkey.xml')
+	local frame = ui.GetFrame('quickslotnexpbar')
+	QUICKSLOTNEXPBAR_UPDATE_HOTKEYNAME(frame);
+	frame:Invalidate()
+	if isChangePos then
+		swapQuickslotPos(idx_1 +1,idx_2 +1)
+	end
+end
 
 function REPLACEMONSTERSKILLSLOT_ON_INIT(addon,frame)
 	g.addon = addon
-    g.frame = frame
+	g.frame = frame
+	g.user = GETMYPCNAME()
     acutil.slashCommand("/monsterskill", REPLACEMONSTERSKILLSLOT_COMMAND);
     acutil.slashCommand("/replacemonsterskillslot", REPLACEMONSTERSKILLSLOT_COMMAND);
     acutil.slashCommand("/rmss", REPLACEMONSTERSKILLSLOT_COMMAND);
 	acutil.setupHook(QUICKSLOTNEXPBAR_MY_MONSTER_SKILL_HOOK,'QUICKSLOTNEXPBAR_MY_MONSTER_SKILL')
-
-	acutil.setupHook(QUICKSLOTNEXPBAR_SLOT_USE_HOOK,"QUICKSLOTNEXPBAR_SLOT_USE")
-	addon:RegisterMsg("BUFF_ADD", "TRANSFORM_BUFF_CHECK")
 	addon:RegisterMsg("GAME_START_3SEC","REPLACEMONSTERSKILLSLOT_INIT")
+end
+
+function REPLACEMONSTERSKILLSLOT_INIT()
+	local frame = ui.GetFrame('keyconfig')
+	KEYCONFIG_OPEN_CATEGORY(frame,'hotkey.xml','Battle')
+	g.setting[g.user] = g.setting[g.user] or {}
+	if g.setting[g.user].transforming then
+		restoreQuickSlotItem()
+		restoreHotkeySetting(g.hoge[g.lastSlotNum],g.foo[g.lastSlotNum],false)
+		g.setting[g.user].transforming = false
+		acutil.saveJSON('../addons/replacemonsterskillslot/setting.json',g.setting)
+	end
+	frame:SetCloseScript('SaveHotKeySetting')
+	SaveHotKeySetting()
 end
 
 function REPLACEMONSTERSKILLSLOT_COMMAND(cmd)
@@ -65,28 +208,36 @@ function QUICKSLOTNEXPBAR_MY_MONSTER_SKILL_HOOK(isOn, monName, buffType)
 
 			slot:EnableDrag(0);
 		end
-		local lastSlot = GET_CHILD_RECURSIVELY(frame, "slot"..(list:Count() +1 +slotNum), "ui::CSlot");
+		local lastSlot = GET_CHILD_RECURSIVELY(frame, "slot"..(list:Count() +1 ), "ui::CSlot");
 		local icon = lastSlot:GetIcon();
 		if icon ~= nil then
 			local iconInfo = icon:GetInfo();
 			lastSlot:SetUserValue('ICON_CATEGORY', iconInfo.category);
 			lastSlot:SetUserValue('ICON_TYPE', iconInfo.type);
-		end
 
+		end
+		swapSlotItems(list:Count()+1,list:Count() +1 +slotNum)
 		CLEAR_SLOT_ITEM_INFO(lastSlot);
 		local icon = CreateIcon(lastSlot);
-		local slotString 	= 'QuickSlotExecute'..(list:Count() +1 +slotNum);
+		local slotString 	= 'QuickSlotExecute'..(list:Count() +1 );
 		local text 			= hotKeyTable.GetHotKeyString(slotString);
 		lastSlot:SetText('{s14}{#f0dcaa}{b}{ol}'..text, 'default', 'left', 'top', 2, 1);
 		icon:SetImage("druid_del_icon");		
 		lastSlot:EnableDrag(0);
 		SET_QUICKSLOT_OVERHEAT(lastSlot);
 		frame:SetUserValue('SKL_MAX_CNT',list:Count() + 1)
-		
+		g.setting[g.user].transforming = true
+		g.lastSlotNum = list:Count()+1
+		swapHotkeySetting(g.hoge[list:Count()+1],g.foo[list:Count()+1],true)
+		acutil.saveJSON('../addons/replacemonsterskillslot/setting.json',g.setting)
 		return;
 	end
 
 	local sklCnt = frame:GetUserIValue('SKL_MAX_CNT');
+	if g.setting[g.user].transforming then
+		restoreQuickSlotItem()
+	end
+	restoreHotkeySetting(g.hoge[sklCnt],g.foo[sklCnt],true)
 	for i = 1 + slotNum, slotNum +  sklCnt do
 		local slot = GET_CHILD_RECURSIVELY(frame, "slot"..i, "ui::CSlot");
 		CLEAR_SLOT_ITEM_INFO(slot);	
@@ -100,158 +251,6 @@ function QUICKSLOTNEXPBAR_MY_MONSTER_SKILL_HOOK(isOn, monName, buffType)
 		slot:SetUserValue('ICON_CATEGORY', 'None');
 		slot:SetUserValue('ICON_TYPE', 0);
 		SET_QUICKSLOT_OVERHEAT(slot)
-
-end
+	end
 	frame:SetUserValue('SKL_MAX_CNT',0)
-end
-
-
-
-function GetHotKeySetting(idx)
-	local obj = {}
-	obj.idx = idx
-	obj.key = config.GetHotKeyElementAttributeForConfig(idx, "Key")
-	obj.useAlt = config.GetHotKeyElementAttributeForConfig(idx, "UseAlt")
-	obj.useCtrl = config.GetHotKeyElementAttributeForConfig(idx, "UseCtrl")
-	obj.useShift = config.GetHotKeyElementAttributeForConfig(idx, "UseShift")
-	print(string.format("idx %d , key %s , alt %s , ctrl %s , shift %s ",obj.idx,obj.key,obj.useAlt,obj.useCtrl,obj.useShift))
-	return obj
-end
-
-function setHotkeySetting(obj,idx)
-	if not idx then idx = obj.idx end
-	config.SetHotKeyElementAttributeForConfig(idx, "Key", obj.key);
-	config.SetHotKeyElementAttributeForConfig(idx, "UseAlt", obj.useAlt);
-	config.SetHotKeyElementAttributeForConfig(idx, "UseCtrl", obj.useCtrl);
-	config.SetHotKeyElementAttributeForConfig(idx, "UseShift", obj.useShift);
-	config.SaveHotKey('hotkey.xml');
-end
-
-function repleceHotkeySetting()
-	for i = 1 , 5 do
-		setHotkeySetting(g.foo[i],g.hoge[i].idx)
-	end
-	local quickSlotFrame = ui.GetFrame("quickslotnexpbar");
-	QUICKSLOTNEXPBAR_UPDATE_HOTKEYNAME(quickSlotFrame);
-	quickSlotFrame:Invalidate();
-end
-
-function restoreHoekeySetting()
-	for i = 1, 5 do
-		setHotkeySetting(g.hoge[i])
-	end
-	local quickSlotFrame = ui.GetFrame("quickslotnexpbar");
-	QUICKSLOTNEXPBAR_UPDATE_HOTKEYNAME(quickSlotFrame);
-	quickSlotFrame:Invalidate();
-end
-
-function REPLACEMONSTERSKILLSLOT_INIT()
-
-	local cid = info.GetCID(session.GetMyHandle())
-    local pcSession = session.GetSessionByCID(cid);
-    local pcJobInfo = pcSession.pcJobInfo;
-    local cnt = pcJobInfo:GetJobCount();
-    local n = 1
-    local tempTable = {}
-    g.presetTable = {}
-    for i = 0 , cnt - 1 do
-        local jobID = pcJobInfo:GetJobByIndex(i);
-        if jobID == -1 then
-            break;
-		end
-		if jobID == 4005 then 
-			g.isDruid = true
-		end
-	end
-
-	local frame = ui.GetFrame('keyconfig')
-	KEYCONFIG_OPEN_CATEGORY(frame,'hotkey.xml','Battle')
-	frame:SetCloseScript('REPLACEMONSTERSKILLSLOT_INIT')
-	-- hoge foo あとで置換する
-	g.hoge = {}
-	for i = 0 , 4 do
-		table.insert(g.hoge,GetHotKeySetting(i))
-	end
-	g.foo = {}
-	for i = g.setting.slotNum , g.setting.slotNum + 4 do
-		table.insert(g.foo , GetHotKeySetting(i))
-	end
-end
-
-function TRANSFORM_BUFF_CHECK(frame, msg, argStr, buffid)
-	if buffid == 6012 then
-		-- restoreHoekeySetting()
-	end
-end
-
-function QUICKSLOTNEXPBAR_SLOT_USE_HOOK(frame, slot, argStr, argNum)
-	if GetCraftState() == 1 then
-		return;
-	end
-
-	if ui.CheckHoldedUI() == true then
-		return;
-	end
-
-	tolua.cast(slot, "ui::CSlot");
-	local icon = slot:GetIcon();
-	if icon == nil then
-		return;
-	end
-
-	local iconInfo = icon:GetInfo();
-
-	if iconInfo.category == 'Skill' then    
-		-- add
-		print(iconInfo.type)
-		if iconInfo.type == 40904 and g.isDruid then
-			repleceHotkeySetting()
-			ReserveScript(repleceHotkeySetting(),0.2)
-		end
-		ICON_USE(icon);
-		return;
-	end
-
-	local invenItemInfo = session.GetInvItem(iconInfo.ext);
-	if invenItemInfo == nil then
-		invenItemInfo = session.GetInvItemByType(iconInfo.type);
-	elseif invenItemInfo.type ~= iconInfo.type then
-		return;
-	end
-
-	if invenItemInfo == nil then
-		if iconInfo.category == 'Item' then
-			icon:SetColorTone("FFFF0000");
-			icon:SetText('0', 'quickiconfont', 'right', 'bottom', -2, 1);
-		end
-		return;
-	end
-
-	local itemobj = GetIES(invenItemInfo:GetObject());
-	if TRY_TO_USE_WARP_ITEM(invenItemInfo, itemobj) == 1 then
-		return;
-	end
-		
-	if invenItemInfo.count == 0 then
-		icon:SetColorTone("FFFF0000");
-		icon:SetText(invenItemInfo.count, 'quickiconfont', 'right', 'bottom', -2, 1);
-		return;
-	end
-		
-	if true == BEING_TRADING_STATE() then
-		return;
-	end
-
-	local invItemAllowReopen = ''
-	if itemobj ~= nil then
-		invItemAllowReopen = TryGetProp(itemobj, 'AllowReopen')
-	end
-
-	local groupName = itemobj.ItemType;
-	local gachaCubeFrame = ui.GetFrame('gacha_cube')
-	if groupName == 'Consume' and gachaCubeFrame ~= nil and gachaCubeFrame:IsVisible() == 1 and invItemAllowReopen == 'YES' then
-		return
-	end
-
-	ICON_USE(icon);
 end
